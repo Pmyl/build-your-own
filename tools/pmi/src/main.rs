6fa401fdfc8e0525efa5236f035177b25c5cc07f
Pmyl <julo134@gmail.com>
2025-11-28 14:16:01 +0000

    Fix entrypoint for pmi binary

--- tools/pmi/src/main.rs
+++ tools/pmi/src/main.rs
@@ -1,10 +1,10 @@
 use std::env;
 
 use build_your_own_utils::my_own_error::MyOwnError;
-use install::install_cli;
+use pmi::pmi_cli;
 
 fn main() -> Result<(), MyOwnError> {
     let args: Vec<String> = env::args().skip(1).collect();
     let args: Vec<&str> = args.iter().map(|s| &**s).collect::<Vec<&str>>();
-    install_cli(&args)
+    pmi_cli(&args)
 }
