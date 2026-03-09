558e97ba49cfd5f5db202f0fece0f375156c87f9
Pmyl <julo134@gmail.com>
2026-03-09 11:21:23 +0000

    pmi: Fix persisted application printing

--- tools/pmi/src/applications.rs
+++ tools/pmi/src/applications.rs
@@ -203,7 +203,7 @@ impl<'a> Display for PersistedApplication<'a> {
             }
 
             for other in others {
-                writeln!(f, "   ...and (through) {}", first.source)?;
+                write!(f, "\n   ...and (through) {}", first.source)?;
                 if !other.args.is_empty() {
                     write!(f, " {}", other.args.join(" "))?;
                 }
